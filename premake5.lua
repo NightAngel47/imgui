91075f53d1cdddfb132a82df030d6b86f1d35633
Steven Drovie <NightAngel47@users.noreply.github.com>
2021-05-01 18:19:11 -0500

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    staticruntime "off"
   
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -22,7 +23,6 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 	
 	filter "configurations:Debug"
 		runtime "Debug"
